8841eaee13bf3394bcbccb96ae2a9e41dd4b4854
Alz-Android <apiran06@gmail.com>
2020-05-28 10:24:08 -0700

    put aside the GenreItem component

--- components/GenreItem.js
+++ components/GenreItem.js
@@ -4,15 +4,16 @@ import {styles} from './../styles/styles.js'
 
 export class GenreItem extends Component { 
     render() {
+      console.log(this.props.action);
       return ( 
         <View style={{flexDirection: 'row'}}>           
-              <ImageBackground  style={styles.imageThumbnailTitles} source={{ uri: this.props.imageURI }} >
-                <Text style={styles.loginText}>{this.props.genre }</Text>
-              </ImageBackground>   
-              <CheckBox
-              value={this.props.state}
-              onChange={() => this.setState({ adventure: !this.props.state })}
-              />                    
+          <ImageBackground  style={styles.imageThumbnailTitles} source={{ uri: this.props.imageURI }} >
+            <Text style={styles.loginText}>{this.props.genre }</Text>
+          </ImageBackground>   
+          <CheckBox
+          value={this.props.action}
+          onChange={() => this.setState({ adventure: !this.props.action })}
+          />                    
         </View>
       );
     }
--- screens/SelectGenresScreen.js
+++ screens/SelectGenresScreen.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { CheckBox, Text, View, ImageBackground, TouchableOpacity} from 'react-native';
 import {styles} from './../styles/styles.js'
-import {GenreItem} from './../components/GenreItem.js'
+// import {GenreItem} from './../components/GenreItem.js'
 
 export class SelectGenresScreen extends React.Component {
     constructor(props) {
       super(props)
+
+      // // Bind the this context to the handler function
+      // this.handler = this.handler.bind(this);
+
       this.state = {
         action: false,
         adventure: false,
@@ -16,27 +20,72 @@ export class SelectGenresScreen extends React.Component {
       }
     }
   
+  //   handler() {
+  //     this.setState({
+  //       // action: true,
+  //       // adventure: true,
+  //       anime: true,
+  //       // horror: true,
+  //       // romance: true,
+  //       // scifi: true,
+  //     });
+  // }
+
+    
+
     render() {
       // it seems as if when I'm using the custom component the info about the checkbox is going in the GenreItem, but it's not being reflected back out of that class.
 
-
       return (
         <View style={styles.container}>  
-          <GenreItem imageURI={'https://i.picsum.photos/id/111/80/80.jpg'} genre={'Action'} state={this.state.action}> </GenreItem>
-          {/* <GenreItem imageURI={'https://i.picsum.photos/id/112/80/80.jpg'} genre={'Adventure'} state={this.state.adventure}> </GenreItem> */}
-          <GenreItem imageURI={'https://i.picsum.photos/id/113/80/80.jpg'} genre={'Anime'} state={this.state.anime}> </GenreItem>
-          <GenreItem imageURI={'https://i.picsum.photos/id/114/80/80.jpg'} genre={'Horror'} state={this.state.horror}> </GenreItem>
-          <GenreItem imageURI={'https://i.picsum.photos/id/115/80/80.jpg'} genre={'Romance'} state={this.state.romance}> </GenreItem>
-          <GenreItem imageURI={'https://i.picsum.photos/id/116/80/80.jpg'} genre={'SciFi'} state={this.state.scifi}> </GenreItem>
+          {/* <GenreItem imageURI={'https://i.picsum.photos/id/111/80/80.jpg'} genre={'Action'} state={this.state.action}> </GenreItem>
+          <GenreItem imageURI={'https://i.picsum.photos/id/112/80/80.jpg'} genre={'Adventure'} state={this.state.adventure}> </GenreItem>
+          <GenreItem imageURI={'https://i.picsum.photos/id/113/80/80.jpg'} genre={'Anime'} action={this.state.anime}> </GenreItem>
+          <GenreItem imageURI={'https://i.picsum.photos/id/114/80/80.jpg'} genre={'Horror'} action={this.state.horror}> </GenreItem>
+          <GenreItem imageURI={'https://i.picsum.photos/id/115/80/80.jpg'} genre={'Romance'} action={this.state.romance}> </GenreItem>
+          <GenreItem imageURI={'https://i.picsum.photos/id/116/80/80.jpg'} genre={'SciFi'} action={this.state.scifi}> </GenreItem> */}
+
+
+          <View style={{flexDirection: 'row'}}>             
+            <ImageBackground  style={styles.imageThumbnailTitles} source={{ uri: 'https://i.picsum.photos/id/111/80/80.jpg' }} >      
+              <Text style={styles.loginText}>Action</Text>
+            </ImageBackground>         
+            <CheckBox
+            value={this.state.action}
+            onChange={() => this.setState({ action: !this.state.action })}
+            />                    
+          </View>
           
           <View style={{flexDirection: 'row'}}>
-            <Text style={styles.loginText}>Adventure</Text>
-            <ImageBackground  style={styles.imageThumbnailTitles} source={{ uri: 'https://i.picsum.photos/id/12/80/80.jpg' }} />
+            <ImageBackground  style={styles.imageThumbnailTitles} source={{ uri: 'https://i.picsum.photos/id/12/80/80.jpg' }} >
+              <Text style={styles.loginText}>Adventure</Text>
+            </ImageBackground>  
             <CheckBox
               value={this.state.adventure}
               onChange={() => this.setState({ adventure: !this.state.adventure })}
             />
           </View>
+          <View style={{flexDirection: 'row'}}>             
+            <ImageBackground  style={styles.imageThumbnailTitles} source={{ uri: 'https://i.picsum.photos/id/113/80/80.jpg' }} >      
+              <Text style={styles.loginText}>Romance</Text>
+            </ImageBackground>         
+            <CheckBox
+            value={this.state.romance}
+            onChange={() => this.setState({ romance: !this.state.romance })}
+            />                    
+          </View>
+          
+          <View style={{flexDirection: 'row'}}>
+            <ImageBackground  style={styles.imageThumbnailTitles} source={{ uri: 'https://i.picsum.photos/id/121/80/80.jpg' }} >
+              <Text style={styles.loginText}>SciFi</Text>
+            </ImageBackground>  
+            <CheckBox
+              value={this.state.scifi}
+              onChange={() => this.setState({ scifi: !this.state.scifi })}
+            />
+          </View>
+
+
           <TouchableOpacity style={styles.loginBtn} 
             onPress={() => this.props.navigation.navigate('SelectNovel',
               {action: this.state.action, 
